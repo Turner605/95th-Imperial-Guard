9af85ca8b2f829273dfd0fb829c407b6d520b7e0
Turner605 <32025166+Turner605@users.noreply.github.com>
2025-12-04 14:29:24 +0000

    Adding my own custom because I can

--- Main/addons/main/xtdGearSquads/zeus.hpp
+++ Main/addons/main/xtdGearSquads/zeus.hpp
@@ -1,13 +1,7 @@
 class Player {
     alwaysSelectable = 0;
     labels = "Player";
-    values[] = {"Turner","Splash", "Commie", "Solvest", "Ranger", "Axe", "Death", "Elusive"};
+    values[] = {"Turner", "Commie"};
     class Turner { label = "Turner"; description = "Turner`s Helmet"; };
-    class Splash { label = "Splash"; description = "Splash`s Helmet"; };
     class Commie { label = "Commie"; description = "Commie`s Helmet"; };
-    class Solvest { label = "Solvest"; description = "Solvest`s Helmet"; };
-    class Ranger { label = "Ranger"; description = "Ranger`s Helmet"; };
-    class Axe { label = "Axe"; description = "Axe`s Helmet"; };
-    class Death { label = "Death"; description = "Death`s"; };
-    class Elusive { label = "Elusive"; description = "Elusive`s"; };
 };
---
+++ Main/addons/players/turner/script_component.hpp
@@ -0,0 +1,40 @@
+#define COMPONENT players_turner
+#define COMPONENT_BEAUTIFIED Players_Turner
+#define PLAYER_NAME turner
+#define PLAYER_NAME_BEAUTIFIED Turner
+
+//OPTIONS: Zeus, Fallen, Unassigned, Basic, One_Zero, One_One, One_Two, One_Three, One_Four, Five_Zero, Six_Zero, Seven_Zero
+#define SQUAD_KEY Zeus
+
+// HELMET OPTIONS 0 (False), 1 (True)
+#define HELMET_BASIC 1
+#define HELMET_MASK 0
+#define PATROL_CAP 0
+#define TANKER_CAP 0
+#define NCO_CAP 0
+#define OFFICER_CAP 0
+#define BOONIE_HAT 0
+
+// UNIFORM OPTIONS 0 (False), 1 (True)
+#define UNIFORM_BASIC 0
+#define UNIFORM_NCO 0
+
+// BACKPACK OPTIONS 0 (False), 1 (True)
+#define BACKPACK_INVISIBLE 0
+#define BACKPACK_LIGHT 0
+#define BACKPACK_STANDARD 0
+#define BACKPACK_MEDIC 0
+#define BACKPACK_HEAVY 0
+#define BACKPACK_ENGINEER 0
+#define BACKPACK_AT 0
+#define BACKPACK_RTO 0
+
+// VEST OPTIONS 0 (False), 1 (True)
+#define VEST_LIGHT_SET 0
+#define VEST_MEDIUM_SET 0
+#define VEST_HEAVY_SET 0
+
+// DO NOT EDIT
+#define PARENTCOMPONENT shared
+#include "\z\AUX_95th_40k_IG_Main\addons\main\script_mod.hpp"
+#include "\z\AUX_95th_40k_IG_Main\addons\main\script_macros.hpp"
